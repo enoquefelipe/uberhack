ecf9f9c8dc969c8d7455b28888f434f8417b7e3d
Enoque Felipe <enoquefelipe@gmail.com>
2018-06-03 09:27:31 -0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,16 @@ import { AppComponent } from './app.component';
 import { ItemLocacaoComponent } from './item-locacao/item-locacao.component';
 import { OptionListComponent } from './option-list/option-list.component';
 import { DetailsComponent } from './details/details.component';
-import { CheckoutComponent } from './checkout/checkout.component'
+import { CheckoutComponent } from './checkout/checkout.component';
+import { FinishedComponent } from './finished/finished.component';
 
 const routes: Routes = [
-
     { path: '', redirectTo: '/types', pathMatch: 'full' },
     { path: 'types', component: ItemLocacaoComponent },
     { path: 'options', component: OptionListComponent },
     { path: 'details', component: DetailsComponent },
-    { path: 'checkout', component: CheckoutComponent }
-
+    { path: 'checkout', component: CheckoutComponent },
+    { path: 'finished', component: FinishedComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { OptionListComponent } from './option-list/option-list.component';
 import { DetailsComponent } from './details/details.component';
 import { PaymentComponent } from './payment/payment.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { FinishedComponent } from './finished/finished.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemLocacaoComponent, OptionListComponent, DetailsComponent, PaymentComponent, CheckoutComponent],
+  declarations: [AppComponent, ItemLocacaoComponent, OptionListComponent, DetailsComponent, PaymentComponent, CheckoutComponent, FinishedComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
